b6512a2b23bacdbc1939734b4054b8c3e5bc178a
Klavdij <klavdij.starman1@gmail.com>
2016-04-04 22:16:18 +0200

    Prva verzija

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>SlabeFore</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -19,10 +20,27 @@
     <![endif]-->
 </head>
 <body>
-<h1>Hello, world!</h1>
+
+<div class="container">
+
+    <div class="page-header">
+        <h1>Bootstrap Navbar</h1>
+        <p class="lead">Nav bar boys</p>
+    </div>
+
+    <div class="content">
+        <div class="post">
+            <p class="lead" id="test">Tvoja mt je res debela - puška 2016</p>
+
+        </div>
+
+    </div>
+
+</div><!-- /.container -->
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 </body>
